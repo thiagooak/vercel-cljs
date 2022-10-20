a08782f5c43206d2ae555bae2118b408ff79588a
Thiago Carvalho <thiago.carvalho2@nubank.com.br>
2022-10-20 15:44:51 -0300

    Try plain js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import { BuildOptions, Lambda, FileBlob } from '@vercel/build-utils';
 
 export const version = 3;
 
-export async function build(options: BuildOptions) {
+export async function build(BuildOptions) {
     const lambda = new Lambda({
-        runtime: 'nodejs16.x',
+        runtime: 'nodejs8.10',
         handler: 'index.main',
         files: {
             'index.js': new FileBlob({ data: 'exports.main = () => { return "Test" }' }),
